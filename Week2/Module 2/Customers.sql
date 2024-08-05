4d637fd54eaf2d3ccf97161566883de4dd00664a
nilanjana07 <nilanjana030807@gmail.com>
2024-08-05 09:54:28 +0530

    customer table

---
+++ Week2/Module 2/Customers.sql
@@ -0,0 +1,34 @@
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (3, 'Robert Harris', TO_DATE('1985-03-15', 'YYYY-MM-DD'), 2200, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (4, 'Emily Johnson', TO_DATE('1992-11-05', 'YYYY-MM-DD'), 3000, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (5, 'Michael Brown', TO_DATE('1988-06-25', 'YYYY-MM-DD'), 1800, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (6, 'Sarah Davis', TO_DATE('1995-02-10', 'YYYY-MM-DD'), 2500, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (7, 'David Wilson', TO_DATE('1982-09-30', 'YYYY-MM-DD'), 3200, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (8, 'Olivia Martinez', TO_DATE('1990-12-12', 'YYYY-MM-DD'), 2700, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (9, 'James Anderson', TO_DATE('1987-04-07', 'YYYY-MM-DD'), 1900, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (10, 'Isabella Taylor', TO_DATE('1993-08-19', 'YYYY-MM-DD'), 2300, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (11, 'Ethan Thomas', TO_DATE('1986-10-22', 'YYYY-MM-DD'), 2100, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (12, 'Sophia Lee', TO_DATE('1994-01-30', 'YYYY-MM-DD'), 2600, SYSDATE);
+
+select * from Customers
+
+
+
