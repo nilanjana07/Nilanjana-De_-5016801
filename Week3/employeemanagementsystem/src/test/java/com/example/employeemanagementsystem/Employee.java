1e75237ba27bca18b95c21712a3863ae4afbb822
nilanjana07 <nilanjana030807@gmail.com>
2024-08-12 23:08:56 +0530

    week3

---
+++ Week3/employeemanagementsystem/src/test/java/com/example/employeemanagementsystem/Employee.java
@@ -0,0 +1,31 @@
+package com.example.employeemanagementsystem;
+
+import org.hibernate.annotations.Table;
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@SuppressWarnings("removal")
+@Entity
+@Table(name = "employees", name = "")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
+
