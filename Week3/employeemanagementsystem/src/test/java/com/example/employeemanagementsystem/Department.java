684448e238a061ecfc98f54fbdbcc7d593e6782b
nilanjana07 <nilanjana030807@gmail.com>
2024-08-14 16:29:11 +0530

    week3

--- Week3/employeemanagementsystem/src/test/java/com/example/employeemanagementsystem/Department.java
+++ Week3/employeemanagementsystem/src/test/java/com/example/employeemanagementsystem/Department.java
@@ -1,5 +1,23 @@
 package com.example.employeemanagementsystem;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
 }
+
