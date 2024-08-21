e031dc750f4324bcec5fa25c202e65d9dfa0480d
nilanjana07 <nilanjana030807@gmail.com>
2024-08-21 20:13:12 +0530

    week4

--- Week4/BookStoreAPI/BookStore/BookStore/src/main/java/com/example/BookStore/repository/BookRepository.java
+++ Week4/BookStoreAPI/BookStore/BookStore/src/main/java/com/example/BookStore/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package com.example.BookStore.repository;
-import com.example.Bookstore.model.Book;
+import com.example.BookStore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
--- Week4/BookStoreAPI/BookStore/BookStore/src/main/java/com/example/BookStore/repository/CustomerRepository.java
+++ Week4/BookStoreAPI/BookStore/BookStore/src/main/java/com/example/BookStore/repository/CustomerRepository.java
@@ -1,5 +1,9 @@
 package com.example.BookStore.repository;
 
-public class CustomerRepository {
-    
+import com.example.BookStore.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
